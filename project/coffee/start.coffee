fcf746795187d3ba5d3239fd794e247f7fd99f6c
Marius Karthaus <marius@karthaus.nl>
2014-11-25 16:17:14 +0100

    Added simple setup for testing, minor fixes

--- project/coffee/start.coffee
+++ project/coffee/start.coffee
@@ -1,2 +1,81 @@
 # Server startup script
-console.log "this still works"
+class YmSmtpStart
+	require('colors');
+
+	constructor:()->
+		@debuglevel=10
+		@name='Youmagine SMTP'
+		# must be root
+		if process.getuid()>1
+			@err "You must be root to start #{@name}"
+			process.exit(1);
+		@debug 1,"Starting #{@name} "
+		@checkUsers()
+		@setupServer()
+		@startHaraka()	
+
+	checkUsers:=>
+		posix = require('posix')
+		try
+			@uid=posix.getpwnam('nobody')
+			@gid=posix.getgrnam('nogroup')
+		catch e
+			# do nothing 
+			@err "I want to drop privileges to nobody/nogroup, but the unix user and group do not exist",e
+			process.exit(1);
+
+
+	setupServer:=>
+		rsync = require('rsync');
+		sync = new rsync().flags('ar').source("#{__dirname}/project/server/").destination("#{__dirname}/server/");
+		sync.execute( (error, code, cmd)=>
+			if error?
+				@err 'Failed to sync server configuration',error,code,cmd
+				process.exit(1);
+			@debug 1,"Synced server configuration"
+		)
+    
+	startHaraka:=>
+		forever = require('forever-monitor');
+		config=
+			max:3 
+			args:['--config',"#{__dirname}/server/"]
+			minUptime:2000
+			spinSleepTime:2000
+			logFile:"#{__dirname}/logs/log.log"
+			outFile:"#{__dirname}/logs/log.out"
+			errFile:"#{__dirname}/logs/log.err"
+			silent:true
+
+		if @debuglevel>3
+			config.silent = false
+
+		child = new (forever.Monitor)("#{__dirname}/node_modules/Haraka/bin/haraka",config)
+		child.on('start', (p,d)=>
+			@debug 3,'Haraka start'.blue,'PID:',d.pid,'FPID:',d.foreverPid
+		);
+		child.on('stop', (p)=>
+			@debug 3,'Haraka stopped'.blue
+		);
+		child.on('restart', ()=>
+			@debug 3,'Haraka restart'.blue,'PID:',child.childData.pid,'FPID:',child.childData.foreverPid
+		);
+		child.on('error', (e)=>
+			@err 'Haraka error'.blue,e,"Check logfiles in logs/ to see what might have happened";
+		);
+		child.on('exit', (c)=>
+			@err 'Haraka ended'.blue,"Check logfiles in logs/ to see what might have happened";
+		);
+		child.start()
+
+	debug:(level,data...)=>
+		if level<=@debuglevel
+			console.error 'YmSmtpStart'.green,data...
+
+	err:(data...)=>
+		if @debuglevel>0
+			console.error 'YmSmtpStart'.red.bgWhite,data...
+
+new YmSmtpStart()
+
+
--- start.js
+++ start.js
@@ -1,6 +1,124 @@
 #!/usr/bin/node
 (function() {
-console.log("this still works");
+var YmSmtpStart,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  __slice = [].slice;
+
+YmSmtpStart = (function() {
+  require('colors');
+
+  function YmSmtpStart() {
+    this.err = __bind(this.err, this);
+    this.debug = __bind(this.debug, this);
+    this.startHaraka = __bind(this.startHaraka, this);
+    this.setupServer = __bind(this.setupServer, this);
+    this.checkUsers = __bind(this.checkUsers, this);
+    this.debuglevel = 10;
+    this.name = 'Youmagine SMTP';
+    if (process.getuid() > 1) {
+      this.err("You must be root to start " + this.name);
+      process.exit(1);
+    }
+    this.debug(1, "Starting " + this.name + " ");
+    this.checkUsers();
+    this.setupServer();
+    this.startHaraka();
+  }
+
+  YmSmtpStart.prototype.checkUsers = function() {
+    var e, posix;
+    posix = require('posix');
+    try {
+      this.uid = posix.getpwnam('nobody');
+      return this.gid = posix.getgrnam('nogroup');
+    } catch (_error) {
+      e = _error;
+      this.err("I want to drop privileges to nobody/nogroup, but the unix user and group do not exist", e);
+      return process.exit(1);
+    }
+  };
+
+  YmSmtpStart.prototype.setupServer = function() {
+    var rsync, sync;
+    rsync = require('rsync');
+    sync = new rsync().flags('ar').source("" + __dirname + "/project/server/").destination("" + __dirname + "/server/");
+    return sync.execute((function(_this) {
+      return function(error, code, cmd) {
+        if (error != null) {
+          _this.err('Failed to sync server configuration', error, code, cmd);
+          process.exit(1);
+        }
+        return _this.debug(1, "Synced server configuration");
+      };
+    })(this));
+  };
+
+  YmSmtpStart.prototype.startHaraka = function() {
+    var child, config, forever;
+    forever = require('forever-monitor');
+    config = {
+      max: 3,
+      args: ['--config', "" + __dirname + "/server/"],
+      minUptime: 2000,
+      spinSleepTime: 2000,
+      logFile: "" + __dirname + "/logs/log.log",
+      outFile: "" + __dirname + "/logs/log.out",
+      errFile: "" + __dirname + "/logs/log.err",
+      silent: true
+    };
+    if (this.debuglevel > 3) {
+      config.silent = false;
+    }
+    child = new forever.Monitor("" + __dirname + "/node_modules/Haraka/bin/haraka", config);
+    child.on('start', (function(_this) {
+      return function(p, d) {
+        return _this.debug(3, 'Haraka start'.blue, 'PID:', d.pid, 'FPID:', d.foreverPid);
+      };
+    })(this));
+    child.on('stop', (function(_this) {
+      return function(p) {
+        return _this.debug(3, 'Haraka stopped'.blue);
+      };
+    })(this));
+    child.on('restart', (function(_this) {
+      return function() {
+        return _this.debug(3, 'Haraka restart'.blue, 'PID:', child.childData.pid, 'FPID:', child.childData.foreverPid);
+      };
+    })(this));
+    child.on('error', (function(_this) {
+      return function(e) {
+        return _this.err('Haraka error'.blue, e, "Check logfiles in logs/ to see what might have happened");
+      };
+    })(this));
+    child.on('exit', (function(_this) {
+      return function(c) {
+        return _this.err('Haraka ended'.blue, "Check logfiles in logs/ to see what might have happened");
+      };
+    })(this));
+    return child.start();
+  };
+
+  YmSmtpStart.prototype.debug = function() {
+    var data, level;
+    level = arguments[0], data = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+    if (level <= this.debuglevel) {
+      return console.error.apply(console, ['YmSmtpStart'.green].concat(__slice.call(data)));
+    }
+  };
+
+  YmSmtpStart.prototype.err = function() {
+    var data;
+    data = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+    if (this.debuglevel > 0) {
+      return console.error.apply(console, ['YmSmtpStart'.red.bgWhite].concat(__slice.call(data)));
+    }
+  };
+
+  return YmSmtpStart;
+
+})();
+
+new YmSmtpStart();
 
 
 }).call(this);
